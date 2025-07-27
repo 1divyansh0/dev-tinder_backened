0415cf7f0eca856bb45d137cfc2d8332d8936071
1divyansh0 <rajvishwa1983@gmail.com>
2025-07-27 19:57:41 +0530

    changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Load from .env
 const mongoose = require("mongoose");
 
 const connect = async () => {
-    await mongoose.connect(process.env.VITE_MONGODB_URI);
+   await mongoose.connect(process.env.VITE_MONGODB_URI);
 };
 
 module.exports = connect;
